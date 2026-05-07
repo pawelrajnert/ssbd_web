8c439243619e5d6afe9711f3ac79f4a7d0d0ab84
rbartczak05 <bartczakremigiusz@gmail.com>
2026-05-08 00:43:32 +0200

    SUM rozszerzenie S2_oauth: Dodanie nowego przycisku do logowania przez google. (#)

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import "./index.css"
 import "./lib/i18n"
+import {GoogleOAuthProvider} from '@react-oauth/google';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App/>
-    </StrictMode>,
+        <GoogleOAuthProvider clientId="233880181990-t0l5j9tq8et8pbpb9n854vtstcuap5t0.apps.googleusercontent.com">
+            <App/>
+        </GoogleOAuthProvider>
+    </StrictMode>
 )
