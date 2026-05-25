a63e4aeb24496eaf24e957f5f0b9f2e218befcce
Mateusz Chodulski <252929@edu.p.lodz.pl>
2026-05-25 21:20:05 +0200

    SSBD & SUM: tworzenie przedmiotu trzeci commit #76

--- src/services/SubjectService.ts
+++ src/services/SubjectService.ts
@@ -3,10 +3,10 @@ import axiosInstance from '../api/auth/middleware';
 
 export const subjectService = {
     createSubject: async (data: SubjectDTO): Promise<void> => {
-        await axiosInstance.post('/api/subjects', data);
+        await axiosInstance.post('/subjects', data);
     },
     getSubjects: async (): Promise<SubjectDTO[]> => {
-        const response = await axiosInstance.get('/api/subjects');
+        const response = await axiosInstance.get('/subjects');
         return response.data;
     }
 };
